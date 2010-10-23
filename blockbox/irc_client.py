e4cfe27933a2e12897aed4cb77d86f30bd2ccdfa
tyteen4a03 <tyteen4a03@hk-diy.net>
2010-10-23 11:48:34 +0800

    Working userColour in irc_client.py, worldowner declaration in server.py

--- blockbox/irc_client.py
+++ blockbox/irc_client.py
@@ -321,7 +321,7 @@ class ChatBot(irc.IRCClient):
 						moddedmsg = msg[:51].replace(" ", "")
 						if moddedmsg[len(moddedmsg)-2] == "&":
 							msg = msg.replace("&", "*")
-					self.factory.queue.put((self, TASK_IRCMESSAGE, (127, userColour(user), user, msg)))
+					self.factory.queue.put((self, TASK_IRCMESSAGE, (127, self.userColour(user), user, msg)))
 		except:
 			self.logger.error(traceback.format_exc())
 			self.msg(self.factory.irc_channel,"ERROR " + traceback.format_exc())
