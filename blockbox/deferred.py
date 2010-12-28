e8415cd449462bd5eb381d40b8b32bfec7ef6108
tyteen4a03 <tyteen4a03@hk-diy.net>
2010-12-29 02:08:58 +0800

    Happy 2011!

--- blockbox/deferred.py
+++ blockbox/deferred.py
@@ -1,5 +1,5 @@
-# blockBox is Copyright 2009-2010 of the Archives Team, the blockBox Team, and the iCraft team.
-# blockBox is licensed under the Creative Commons by-nc-sa 3.0 UnPorted,
+# blockBox is copyright 2009-2011 the Archives Team, the blockBox Team, and the iCraft team.
+# blockBox is licensed under the Creative Commons by-nc-sa 3.0 UnPorted.
 # To view more details, please see the "LICENSING" file in the "docs" folder of the blockBox Package.
 
 class Deferred(object):
