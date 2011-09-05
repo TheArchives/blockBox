8913d6c9d21f2a8996dcaf59f3c6d8dd65b94390
tyteen4a03 <tyteen4a03@hk-diy.net>
2011-09-05 23:40:56 +0800

    License header changes

--- blockbox/plugins/lsystem.py
+++ blockbox/plugins/lsystem.py
@@ -1,5 +1,5 @@
-# blockBox is copyright 2009-2011 the Archives Team, the blockBox Team, and the iCraft team.
-# blockBox is licensed under the Creative Commons by-nc-sa 3.0 UnPorted License.
+# blockBox is copyright 2009-2011 the Arc Team, the blockBox Team and other contributors.
+# blockBox is licensed under the BSD 3-Clause Modified License.
 # To view more details, please see the "LICENSING" file in the "docs" folder of the blockBox Package.
 
 import math
